9242500f6e767066e3219229e396856f6fb4453f
Randall Teves <rteves0914@gmail.com>
2021-01-26 08:11:26 -0800

    edit html code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -25,3 +25,38 @@ data.forEach(function(sighting) {
    })
 });
 
+// Create variable that holds 'Enter a Date' text/form
+var dateForm = d3.select("#form");
+
+// Create a variable that will hold 'Filter Table' button
+var filterButton = d3.select("#button");
+
+// Create event handlers for clicking the button or pressing the enter key
+dateForm.on("click", runFiltering);
+filterButton.on("submit",runFiltering);
+
+// Define the function 'runFiltering' using function notation or anonymous notation
+function runFiltering() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Print the value to the console
+    console.log(inputValue);
+
+    // // Create a filtering function that will take dates equal to 'inputElement'
+    // function filterDate(date) {
+    //     return date.datetime === inputValue;
+    // }
+  
+    // Now apply the filter 
+    var newTableFiltered = tableData.filter(year => year.date === inputValue);
+
+    console.log(newTableFiltered);
+};
